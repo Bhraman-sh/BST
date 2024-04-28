187a37a31e7dd061dfec454d605503880b341bf6
Bhraman <bhramanshrestha71@gmail.com>
2024-04-28 21:49:20 +0545

    added largest and smallest finding function

--- include/bst.h
+++ include/bst.h
@@ -18,9 +18,7 @@ public:
     std::cout << "Code management " << std::endl;
   }
 
-  void insert(T n_data) {  
-    insert(n_data, &head);
-  }
+  void insert(T n_data) { insert(n_data, &head); }
 
   void insert(T n_data, Node<T> **ptr) {
     if (*ptr == NULL) {
@@ -28,7 +26,7 @@ public:
       (*ptr)->data = n_data;
       (*ptr)->right = NULL;
       (*ptr)->left = NULL;
-      std::cout << n_data << " was inserted " << std::endl;
+     // std::cout << n_data << " was inserted " << std::endl;
       return;
     }
     if (n_data > (*ptr)->data)
@@ -40,7 +38,10 @@ public:
     }
   }
 
-  void printiot() { printiot(head); }
+  void printiot() {
+    printiot(head);
+    std::cout << std::endl;
+  }
 
   void printiot(Node<T> *ptr) {
     if (ptr == NULL)
@@ -53,6 +54,51 @@ public:
     printiot(ptr->right);
   }
 
+  void printpot() {
+    printpot(head);
+    std::cout << std::endl;
+  }
+
+  void printpot(Node<T> *ptr) {
+    if (ptr == NULL)
+      return;
+
+    std::cout << ptr->data << " ";
+
+    printpot(ptr->left);
+
+    printpot(ptr->right);
+  }
+
+  int findLargest() {
+      return findLargest(head);
+  }
+
+  int findLargest(Node<T> *ptr) {
+      if ( ptr->right == NULL)
+          return ptr->data;
+
+      return findLargest(ptr->right);
+  }
+
+int findSmallest() {
+      return findSmallest(head);
+  }
+
+  int findSmallest(Node<T> *ptr) {
+      if ( ptr->left == NULL)
+          return ptr->data;
+
+      return findSmallest(ptr->left);
+  }
+  void deleteTree(int d_data) {
+    deleteTree(d_data, head);
+  }
+  
+  void deleteTree(int d_data, Node<T> *&ptr) {
+    while (ptr->data != d_data) {
+    }
+  }
 private:
   Node<T> *head;
 };
--- main.cpp
+++ main.cpp
@@ -8,11 +8,18 @@
 int main() {
   BST<int> g1;
   g1.printer();
+  g1.insert(40);
+  g1.insert(20);
+  g1.insert(60);
   g1.insert(10);
-  g1.insert(45);
-  g1.insert(5);
+  g1.insert(30);
+  g1.insert(50);
+  g1.insert(70);
 
+  std::cout << g1.findLargest() << std::endl;
+  std::cout << g1.findSmallest() << std::endl;
   g1.printiot();
+  g1.printpot();
 
   return 0;
 }
