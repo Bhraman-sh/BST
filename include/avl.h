62f0f740e810e5ff98958659c05800a2e88806e9
Bhraman <bhramanshrestha71@gmail.com>
2024-04-29 22:23:00 +0545

    ADD BALANCING FEATURES

--- include/avl.h
+++ include/avl.h
@@ -18,10 +18,61 @@ public:
 
     imbalancedNodeFinder(BST<T>::head);
 
+    if (badBalance != NULL) {
+      rotateAndBalance(n_data);
+
+      badBalance = NULL;
+    }
     // badBalance = NULL;
   }
   // PRIVATE FUNCTION
 private:
+  void rotateAndBalance(int n_data) {
+    if (n_data < (*badBalance)->data) {
+      if (n_data < (*badBalance)->left->data)
+        LL_rotation();
+      else
+        LR_rotation();
+    } else {
+      if (n_data < (*badBalance)->right->data)
+        RL_rotation();
+      else
+        RR_rotation();
+    }
+  }
+
+  void LR_rotation() {
+    Node<T> *&ptr = (*badBalance)->left;
+    Node<T> *temp = ptr;
+    ptr = ptr->right;
+    Node<T> *temp1 = ptr->left;
+    ptr->left = temp;
+    temp->right = temp1;
+    LL_rotation();
+  }
+  void LL_rotation() {
+    Node<T> *temp = *badBalance;
+    *badBalance = (*badBalance)->left;
+    Node<T> *temp1 = (*badBalance)->right;
+    (*badBalance)->right = temp;
+    temp->left = temp1;
+  }
+  void RR_rotation() {
+    Node<T> *temp = (*badBalance);
+    (*badBalance) = (*badBalance)->right;
+    Node<T> *temp1 = (*badBalance)->left;
+    (*badBalance)->left = temp;
+    temp->right = temp1;
+  }
+  void RL_rotation() {
+    Node<T> *&ptr = (*badBalance)->right;
+    Node<T> *temp = ptr;
+    ptr = ptr->left;
+    Node<T> *temp1 = ptr->right;
+    ptr->right = temp;
+    temp->left = temp1;
+    RR_rotation();
+  }
   void imbalancedNodeFinder(Node<T> *&ptr) {
     if (ptr == NULL)
       return;
--- main.cpp
+++ main.cpp
@@ -2,45 +2,58 @@
 
 // C++ standard libraries
 
-#include "bst.h"
 #include "avl.h"
+#include "bst.h"
 #include <iostream>
 
 int main() {
-  BST<int> g1;
-  AVL<int> a1;
-  g1.printer();
-  g1.insert(40);
-  g1.insert(20);
-  g1.insert(60);
-  g1.insert(10);
-  g1.insert(30);
-  g1.insert(50);
-  g1.insert(70);
+  /* BST<int> g1;
+   AVL<int> a1;
+   g1.printer();
+   g1.insert(40);
+   g1.insert(20);
+   g1.insert(60);
+   g1.insert(10);
+   g1.insert(30);
+   g1.insert(50);
+   g1.insert(70);
 
-  std::cout << g1.findLargest() << std::endl;
-  std::cout << g1.findSmallest() << std::endl;
-  std::cout << "Tree before deletion: " << std::endl;
-  g1.printiot();
-  g1.printpot();
+   std::cout << g1.findLargest() << std::endl;
+   std::cout << g1.findSmallest() << std::endl;
+   std::cout << "Tree before deletion: " << std::endl;
+   g1.printiot();
+   g1.printpot();
 
-  g1.deleteTree(40);
-  std::cout << "Tree after deletion: " << std::endl;
-  g1.printiot();
-  g1.printpot();
+   g1.deleteTree(40);
+   std::cout << "Tree after deletion: " << std::endl;
+   g1.printiot();
+   g1.printpot();
 
-  std::cout << "Height of the tree = " << g1.height() << std::endl;
+   std::cout << "Height of the tree = " << g1.height() << std::endl;
 
-  std::cout << "\nFor Avl Tree" << std::endl;
-  a1.printer();
+   std::cout << "\nFor Avl Tree" << std::endl;
+   a1.printer();
 
-  a1.insert(44);
-  a1.insert(55);
-  a1.insert(33);
+   a1.insert(44);
+   a1.insert(55);
+   a1.insert(33);
 
-  std::cout << a1.findLargest() << " " << a1.findSmallest() << std::endl;
-  a1.printiot();
-  a1.printpot();
+   std::cout << a1.findLargest() << " " << a1.findSmallest() << std::endl;
+   a1.printiot();
+   a1.printpot();
+ */
 
+    AVL<int> a1;
+    a1.insert(50);
+    a1.printiot();
+    a1.printpot();
+
+    a1.insert(45);
+    a1.printiot();
+    a1.printpot();
+
+    a1.insert(47);
+    a1.printiot();
+    a1.printpot();
   return 0;
 }
